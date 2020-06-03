6e867961bc6277b4a6640ecee09dd4a5fd33f269
EAVinogradov <e.a.vinogradov@icloud.com>
2020-06-03 16:48:51 +0300

    Добавьте файлы проекта.

---
+++ ExamWork.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAVinogradov.CSWindowsForms.ExamWork
+{
+    public partial class ExamWork : Form
+    {
+        public ExamWork()
+        {
+            InitializeComponent();
+        }
+        string s;
+        int firstDig;
+        int secondDig;
+        int summa;
+        int summaSquare;
+        int count;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox1.Text;
+            firstDig = Convert.ToInt32(s);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox2.Text;
+            secondDig = Convert.ToInt32(s);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            summa = firstDig + secondDig;
+            textBox3.Text = Convert.ToString(summa);
+            summaSquare = firstDig * firstDig + secondDig * secondDig;
+            textBox4.Text = Convert.ToString(summaSquare);
+            if (firstDig > secondDig)
+            {
+                textBox5.Text = Convert.ToString(firstDig);
+            }
+            else
+            {
+                textBox5.Text = Convert.ToString(secondDig);
+            }
+            do
+            {
+                summa++;
+                count++;
+            }
+            while (summa < summaSquare);
+            textBox6.Text = Convert.ToString(count);
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Данное поле должно содержать цифры");
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Данное поле должно содержать цифры");
+            }
+        }
+
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAVinogradov.CSWindowsForms.ExamWork
+{
+    public partial class Time : UserControl
+    {
+        public Time()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+        public bool TimeEnabled
+        {
+            get { return timer1.Enabled; }
+            set { timer1.Enabled = value; }
+        }
+    }
+}
